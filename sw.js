2d08a05cf9bedb1053aa719f106b5faf7d620c14
rhamza783 <70859024+rhamza783@users.noreply.github.com>
2025-12-28 15:40:03 +0500

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'billing-app-v1';
+const ASSETS = [
+    './index.html',
+    './manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://fonts.googleapis.com/css2?family=Noto+Nastaliq+Urdu:wght@400;700&display=swap'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+});
